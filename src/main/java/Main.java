ecdfa12e53e4a8bb1b43fd922eca2d43f8261b6f
Vitalii <vitalii@v-puzko-tmp.local>
2022-05-06 09:04:20 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/vitalii/IdeaProjects/wd/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://conquestador.com/en-int/login");
+
+        WebElement mail = driver.findElement(By.xpath("//*[@id=\"page-container\"]/div[1]/div/form/div[1]/div[1]/div/span/input"));
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"page-container\"]/div[1]/div/form/div[1]/div[2]/div/span/input"));
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"page-container\"]/div[1]/div/form/div[2]/div/div/button"));
+
+        mail.click();
+        mail.sendKeys("ivanaleksandrov2012@gmail.com");
+        password.click();
+        password.sendKeys("qqq111qqq");
+        button.click();
+
+        Thread.sleep(4000);
+
+        driver.quit();
+
+    }
+}
